9ee829fa839ffc1d551616c48265db8f72f31a7f
Aman Gupta <95014109+Amangupta386@users.noreply.github.com>
2023-08-04 07:52:36 +0530

    Create bookAllocationProblem.cpp

---
+++ recursion/bookAllocationProblem.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector<int>& books, int numStudents, int maxPages) {
+    int students = 1;
+    int currentPages = 0;
+
+    for (int i = 0; i < books.size(); i++) {
+        if (books[i] > maxPages) {
+            return false; // A book has more pages than maxPages, not possible
+        }
+
+        if (currentPages + books[i] > maxPages) {
+            students++;
+            currentPages = books[i];
+
+            if (students > numStudents) {
+                return false; // Too many students required, not possible
+            }
+        } else {
+            currentPages += books[i];
+        }
+    }
+
+    return true; // Allocation is possible with numStudents and maxPages
+}
+
+int bookAllocationHelper(vector<int>& books, int numStudents, int left, int right) {
+    if (left > right) {
+        return -1;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    if (isPossible(books, numStudents, mid)) {
+        // If allocation is possible with mid as maximum pages,
+        // try to minimize the maximum pages
+        int leftResult = bookAllocationHelper(books, numStudents, left, mid - 1);
+        if (leftResult != -1) {
+            return leftResult;
+        } else {
+            return mid;
+        }
+    } else {
+        // If allocation is not possible with mid as maximum pages,
+        // try a greater value of maxPages
+        return bookAllocationHelper(books, numStudents, mid + 1, right);
+    }
+}
+
+int bookAllocation(vector<int>& books, int numStudents) {
+    int n = books.size();
+    if (n < numStudents) {
+        return -1; // More students required than books, not possible
+    }
+
+    int totalPages = 0;
+    for (int i = 0; i < n; i++) {
+        totalPages += books[i];
+    }
+
+    int left = 0; // Minimum number of pages a student can read
+    int right = totalPages; // Maximum number of pages a student can read
+    return bookAllocationHelper(books, numStudents, left, right);
+}
+
+int main() {
+    int n, numStudents;
+    cout << "Enter the number of books: ";
+    cin >> n;
+
+    vector<int> books(n);
+    cout << "Enter the number of pages in each book: ";
+    for (int i = 0; i < n; i++) {
+        cin >> books[i];
+    }
+
+    cout << "Enter the number of students: ";
+    cin >> numStudents;
+
+    int result = bookAllocation(books, numStudents);
+    if (result == -1) {
+        cout << "Not possible to allocate books with given number of students." << endl;
+    } else {
+        cout << "Minimum number of pages a student needs to read: " << result << endl;
+    }
+
+    return 0;
+}
